52659e037444b4fd9a24d174390f926f18ff1832
Molnár József <mojo12984@gmail.com>
2025-04-06 20:20:58 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using AutoUpdaterDotNET;
 
 namespace TheMysteriousTower;
 
@@ -22,5 +23,39 @@ public partial class Form1 : Form
 
         blazor.RootComponents.Add<Main>("#app");
         Controls.Add(blazor);
+
+        // Set the URL to your update XML file
+        AutoUpdater.Start("https://raw.githubusercontent.com/Pepice23/TheMysteriousTower/main/update.xml");
+
+        // Optional: Set update check interval (in minutes)
+        AutoUpdater.CheckForUpdateEvent += AutoUpdaterOnCheckForUpdateEvent;
+        AutoUpdater.LetUserSelectRemindLater = true;
+        AutoUpdater.RemindLaterTimeSpan = RemindLaterFormat.Days;
+        AutoUpdater.RemindLaterAt = 2; // Remind after 2 days
+    }
+
+    private void AutoUpdaterOnCheckForUpdateEvent(UpdateInfoEventArgs args)
+    {
+        if (args.IsUpdateAvailable)
+        {
+            DialogResult dialogResult = MessageBox.Show(
+                $@"There is a new version {args.CurrentVersion} available. You currently have version {args.InstalledVersion}. Do you want to update now?",
+                @"Update Available",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information);
+
+            if (dialogResult.Equals(DialogResult.Yes))
+            {
+                try
+                {
+                    AutoUpdater.DownloadUpdate(args);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message, exception.GetType().ToString(), MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+        }
     }
 }
