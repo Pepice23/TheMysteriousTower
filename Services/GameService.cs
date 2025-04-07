fa96dc8b3738d460663ee14d1aa115bee15b544e
Molnár József <mojo12984@gmail.com>
2025-04-07 11:19:13 +0200

    Create GameService.cs

---
+++ Services/GameService.cs
@@ -0,0 +1,25 @@
+using TheMysteriousTower.Helpers;
+
+namespace TheMysteriousTower.Services;
+
+public class GameService{
+
+    public GameService(){
+    }
+    public event Action? OnChange;
+
+    public string BackgroundImage { get; set; } = "/images/backgrounds/bg1.png";
+    public byte FloorNumber { get; set; } = 1;
+
+    public void IncrementFloor(){
+        FloorNumber++;
+        SetBackgroundImage();
+        OnChange?.Invoke();
+    }
+
+    private void SetBackgroundImage(){
+        BackgroundImage = ImageUtility.GetRandomImageFromFolder("images/backgrounds") ?? "";
+        OnChange?.Invoke();
+    }
+
+}
