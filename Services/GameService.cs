6438f730ed27222b9f9fad1e16df9313c19313f9
Molnár József <mojo12984@gmail.com>
2025-04-07 12:52:44 +0200

    Added EnemyCounter and its functionality

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -4,25 +4,53 @@ namespace TheMysteriousTower.Services;
 
 public class GameService
 {
-
-    public GameService()
-    {
-    }
     public event Action? OnChange;
 
     public string BackgroundImage { get; set; } = "/images/backgrounds/bg1.png";
     public byte FloorNumber { get; set; } = 1;
+    public byte CurrentEnemy { get; set; } = 1;
+    public byte MaxEnemy { get; set; } = 7;
 
     public void IncrementFloor()
     {
         FloorNumber++;
         SetBackgroundImage();
+        ResetEnemy();
+        SetMaxEnemy();
         OnChange?.Invoke();
     }
 
     private void SetBackgroundImage()
     {
         BackgroundImage = ImageUtility.GetRandomImageFromFolder("images/backgrounds") ?? "";
+        OnChange?.Invoke();
+    }
+
+    private void SetMaxEnemy()
+    {
+        if (FloorNumber < 10)
+        {
+            MaxEnemy = 7;
+        }
+        else if (FloorNumber < 30)
+        {
+            MaxEnemy = 10;
+        }
+        else
+        {
+            MaxEnemy = 15;
+        }
+    }
+
+    public void IncrementEnemy()
+    {
+        CurrentEnemy++;
+        OnChange?.Invoke();
+    }
+
+    public void ResetEnemy()
+    {
+        CurrentEnemy = 1;
         OnChange?.Invoke();
     }
 
